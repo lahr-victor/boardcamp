a70207dbbb7846b5dc12736a016523a8ddafac09
Victor Lahr <lahr.victor@gmail.com>
2023-05-17 12:30:28 -0300

    feat: implement register rental POST request

--- src/controllers/rentals.controller.js
+++ src/controllers/rentals.controller.js
@@ -5,7 +5,7 @@ import dayjs from 'dayjs';
 import db from '../database/database.connection.js';
 
 // VALUE EXPORTS
-export default async function retrieveRentals(req, res) {
+export async function retrieveRentals(req, res) {
   try {
     const rentals = await db.query(`
     SELECT rentals.*, customers.name AS "customerName", games.name AS "gameName" FROM rentals
@@ -33,5 +33,66 @@ export default async function retrieveRentals(req, res) {
     })));
   } catch (error) {
     res.status(500).send(error.message);
+  }
+}
+
+export async function registerRental(req, res) {
+  const {
+    customerId,
+    gameId,
+    daysRented,
+  } = req.body;
+
+  try {
+    const customer = await db.query(`
+      SELECT * FROM customers WHERE id = $1;
+    `, [customerId]);
+    if (!customer.rows[0]) return res.sendStatus(400);
+
+    const game = await db.query(`
+      SELECT * FROM games WHERE id = $1;
+    `, [gameId]);
+    if (!game.rows[0]) return res.sendStatus(400);
+
+    if (daysRented <= 0) return res.sendStatus(400);
+
+    const rentedGames = await db.query(`
+      SELECT * FROM rentals WHERE "gameId" = $1 AND "returnDate" IS NULL;
+    `, [gameId]);
+    if (game.rows[0].stockTotal < rentedGames.rowCount) return res.sendStatus(400);
+
+    const { pricePerDay } = game.rows[0];
+
+    await db.query(`
+      INSERT INTO rentals (
+        "customerId",
+        "gameId",
+        "daysRented",
+        "rentDate",
+        "originalPrice",
+        "returnDate",
+        "delayFee"
+      )
+      VALUES (
+        $1,
+        $2,
+        $3,
+        $4,
+        $5,
+        $6,
+        $7
+      );
+`, [
+      customerId,
+      gameId,
+      daysRented,
+      dayjs().format('YYYY-MM-DD'),
+      (daysRented * pricePerDay),
+      null,
+      null,
+    ]);
+    return res.sendStatus(201);
+  } catch (error) {
+    return res.status(500).send(error.message);
   }
 }
--- src/routes/rentals.route.js
+++ src/routes/rentals.route.js
@@ -2,13 +2,17 @@
 import { Router } from 'express';
 
 // VALUE IMPORTS
-import retrieveRentals from '../controllers/rentals.controller.js';
+import {
+  retrieveRentals,
+  registerRental,
+} from '../controllers/rentals.controller.js';
 
 // GLOBAL CONSTANTS
 const rentalsRouter = Router();
 
 // FUNCTIONS
 rentalsRouter.get('/rentals', retrieveRentals);
+rentalsRouter.post('/rentals', registerRental);
 
 // VALUE EXPORTS
 export default rentalsRouter;
